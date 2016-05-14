1beebc027f2fdbc6bb6a334994e0612b20e315e9
pa-w <paw@paw.mx>
2016-05-14 14:34:42 -0400

    completed the basic visualization...

--- js/cdmx.js
+++ js/cdmx.js
@@ -24,6 +24,18 @@ $(document).ready (function () {
 				key: "stdin",
 				plot: "points",
 				enumerator: "geometries"
+			},
+			latest: {
+				type: d3.csv,
+				url: "data/aire/latests.csv",
+				id: "latest",
+				processor: function (rows) { 
+					rows = rows.sort (function (a, b) { return d3.ascending (parseInt (a.indice), parseInt (b.indice)); })
+					rows.forEach (function (a) { console.log (a.indice); });
+					var nest = new Nestify (rows, ["clave"], ["clave", "delegacion", "calidad", "parametro", "indice"]);
+
+					return nest.data;
+				}
 			}
 		},
 		prequantifiers: {},
@@ -31,6 +43,7 @@ $(document).ready (function () {
 			maps: {
 				categorize: function (e) {
 					if (e.properties.zmvm !== undefined) {
+					/* municipios */
 						var ent, zmvm = e.properties.zmvm ? "zmvm" : "";
 						switch (e.properties.cve_ent) { 
 							case "09": ent = "cdmx"; break;
@@ -39,21 +52,37 @@ $(document).ready (function () {
 						}
 						return {"class": ent + " " + zmvm + " mun_" + e.properties.cvegeo };
 					} else if (e.properties.clave !== undefined) {
+					/* estaciones de monitoreo */
 						var data = {parse: [
-							{"control_element": ".pt", "element_attrs": {"r": 0} },
+							//{"control_element": ".pt", "element_attrs": {"r": 0} },
 							{"control_element": "." + e.properties.clave + "_1", "element_attrs": {"r": 2} },
 							{"control_element": "." + e.properties.clave + "_2", "element_attrs": {"r": 1} },
 							{"control_element": "." + e.properties.clave + "_3", "element_attrs": {"r": 0.5} }
 						]};
-						return {"r": 3, "data": data} 
+						return {"r": 3, "class": "estacion"} 
 					} else if (e.properties.simat !== undefined ) {
-						var scale = d3.scale.quantize ().domain ([0, 0.5]).range ([1, 2, 3, 4, 5, 6])
-							cls = "pt ";
-
+					/* grid */
+						/* 
+						which stations have an influence on this point: 1 to 5
+						*/
+						var scale = d3.scale.quantize ().range ([1, 2, 3])
+							cls = "pt ", indices = [];
 						for (var s in e.properties.simat) {
-							cls += " " + e.properties.simat [s].cve + "_" + scale (e.properties.simat [s].dist);
+							if (this.data.latest [e.properties.simat [s].cve]) {
+								var index = this.data.latest [e.properties.simat [s].cve].indice.value,
+									domain = d3.scale.linear ().domain ([0, 150]);
+
+								scale.domain ([0, domain (index)]);
+								indices.push (index / scale (e.properties.simat [s].dist));
+							
+								cls += " " + e.properties.simat [s].cve + "_" + scale (e.properties.simat [s].dist);
+							}
+
 						}
-						return {"r": 0.5, "class": cls}
+						var maxIndex = Math.max.apply (null, indices);
+						scale.domain ([0, 100]).range ([0, 2, 4, 5, 6]); // 80 is the max
+
+						return {"r": scale (maxIndex) - 1, "class": cls}
 					}
 				}
 			}
