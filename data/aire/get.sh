62a5c2cc1e300f232fe8b37523ab73ee09b9639c
Paola Villarreal <paw@paw.mx>
2016-05-14 13:41:21 -0500

    Refinement of the fetching bash script

--- data/aire/get.sh
+++ data/aire/get.sh
@@ -3,5 +3,6 @@ DATE=$(`echo date +%Y-%m-%d.%H%M`)
 cd /home/bots/cdmx/data/
 /usr/bin/phantomjs scripts/phantom.aire.cdmx.js > aire/latests.csv; 
 cp aire/latests.csv "aire/$DATE.csv"; 
-git commit -a -m "Data from $DATE"
+git add "aire/$DATE.csv"
+git commit -m "Data from $DATE"
 git push
