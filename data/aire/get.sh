89f708c69589e53d1454f97f7e697354b9dd236a
Paola Villarreal <paw@paw.mx>
2016-05-14 13:39:01 -0500

    Data from 2016-05-14.1339

--- data/aire/get.sh
+++ data/aire/get.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 DATE=$(`echo date +%Y-%m-%d.%H%M`)
-/usr/bin/phantomjs /home/bots/cdmx/data/scripts/phantom.aire.cdmx.js > /home/bots/cdmx/data/aire/latests.csv; cp /home/bots/cdmx/data/aire/latests.csv "/home/bots/cdmx/data/aire/$DATE.csv"; git commit -a -m "Data from $DATE" && git push
+cd /home/bots/cdmx/data/
+/usr/bin/phantomjs data/scripts/phantom.aire.cdmx.js > aire/latests.csv; 
+cp aire/latests.csv "aire/$DATE.csv"; 
+git commit -a -m "Data from $DATE"
+git push
