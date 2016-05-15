8fc581cc2e428314b74f7dd81f0abd7a1bb94827
Paola Villarreal <paw@paw.mx>
2016-05-15 16:38:44 -0500

    added CSV header to the index file

--- data/aire/get.sh
+++ data/aire/get.sh
@@ -3,7 +3,8 @@ DATE=$(`echo date +%Y-%m-%d.%H%M`)
 cd /home/bots/cdmx/
 /usr/bin/phantomjs data/scripts/phantom.aire.cdmx.js > data/aire/latests.csv; 
 cp data/aire/latests.csv "data/aire/$DATE.csv"; 
-find data/aire/ -name "*.csv" | sort -r > data/aire.csv
+echo "index" > data/aire.csv
+find data/aire/ -name "*.csv" | sort -r >> data/aire.csv
 GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git add "data/aire/$DATE.csv" data/aire/latests.csv data/aire.csv
 GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git commit -m "Data from $DATE"
 GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git push
