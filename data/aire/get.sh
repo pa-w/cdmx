c074401a4f5eeed8fc63e49793667bdaf720c911
Paola Villarreal <paw@paw.mx>
2016-05-15 16:22:35 -0500

    modified fetch script

--- data/aire/get.sh
+++ data/aire/get.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 DATE=$(`echo date +%Y-%m-%d.%H%M`)
-cd /home/bots/cdmx/data/
-/usr/bin/phantomjs scripts/phantom.aire.cdmx.js > aire/latests.csv; 
-cp aire/latests.csv "aire/$DATE.csv"; 
-GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git add "aire/$DATE.csv" aire/latests.csv
+cd /home/bots/cdmx/
+/usr/bin/phantomjs data/scripts/phantom.aire.cdmx.js > data/aire/latests.csv; 
+cp data/aire/latests.csv "data/aire/$DATE.csv"; 
+find data/aire/ -name "*.csv" | sort -r > data/aire.csv
+GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git add "data/aire/$DATE.csv" data/aire/latests.csv data/aire.csv
 GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git commit -m "Data from $DATE"
 GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git push
 
