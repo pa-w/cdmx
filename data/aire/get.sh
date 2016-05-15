29167c7c5bbd77f2536da961e89fe19fc890a0e0
Paola Villarreal <paw@paw.mx>
2016-05-15 09:24:29 -0500

    Modified get.sh

--- data/aire/get.sh
+++ data/aire/get.sh
@@ -3,6 +3,6 @@ DATE=$(`echo date +%Y-%m-%d.%H%M`)
 cd /home/bots/cdmx/data/
 /usr/bin/phantomjs scripts/phantom.aire.cdmx.js > aire/latests.csv; 
 cp aire/latests.csv "aire/$DATE.csv"; 
-git add "aire/$DATE.csv"
-git commit -m "Data from $DATE"
-git push
+GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git add "aire/$DATE.csv" aire/latests.csv
+GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git commit -m "Data from $DATE"
+GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git push
