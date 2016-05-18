888720d50c227bfae118beaf608c8b6a0191ba17
Paola Villarreal <paw@paw.mx>
2016-05-17 19:58:08 -0500

    deleted latests and changed it from the script

--- data/aire/get.sh
+++ data/aire/get.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 DATE=$(`echo date +%Y-%m-%d.%H%M`)
 cd /home/bots/cdmx/
-/usr/bin/phantomjs data/scripts/phantom.aire.cdmx.js > data/aire/latests.csv; 
-cp data/aire/latests.csv "data/aire/$DATE.csv"; 
+/usr/bin/phantomjs data/scripts/phantom.aire.cdmx.js > "data/aire/$DATE.csv"; 
 echo "index" > data/aire.csv
 find data/aire/ -name "*.csv" | sort -r >> data/aire.csv
 #GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git add "data/aire/$DATE.csv" data/aire.csv
