0cde2e7d6e96b9edabcad579d524a99a4be81c5d
Paola Villarreal <paw@epicwin.mx>
2016-05-19 18:52:25 -0500

    Changed the commit config

--- data/aire/get.sh
+++ data/aire/get.sh
@@ -4,6 +4,7 @@ cd /home/bots/cdmx/
 /usr/bin/phantomjs data/scripts/phantom.aire.cdmx.js > "data/aire/$DATE.csv"; 
 echo "index" > data/aire.csv
 find data/aire/ -name "*.csv" | sort -r | head -n 100 >> data/aire.csv
+GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git config user.email "paw@epicwin.mx"
 GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git add "data/aire/$DATE.csv" data/aire.csv
 GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git commit -m "Data from $DATE"
 GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git push
