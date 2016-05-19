624248e98393fa4e51cd46427510c5d5b0604f6c
Paola Villarreal <paw@paw.mx>
2016-05-18 22:49:16 -0500

    added index limit to 100 files

--- data/aire/get.sh
+++ data/aire/get.sh
@@ -3,7 +3,7 @@ DATE=$(`echo date +%Y-%m-%d.%H%M`)
 cd /home/bots/cdmx/
 /usr/bin/phantomjs data/scripts/phantom.aire.cdmx.js > "data/aire/$DATE.csv"; 
 echo "index" > data/aire.csv
-find data/aire/ -name "*.csv" | sort -r >> data/aire.csv
+find data/aire/ -name "*.csv" | sort -r | head -n 100 >> data/aire.csv
 #GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git add "data/aire/$DATE.csv" data/aire.csv
 #GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git commit -m "Data from $DATE"
 #GIT_SSH_COMMAND='ssh -i /root/.ssh/paw-bot.id_rsa' git push
